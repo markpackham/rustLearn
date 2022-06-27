496fc0b4652d839ea7dd38eac271dc39c32d20c6
mark <markpackham1@gmail.com>
2022-06-27 09:25:54 +0100

    demo positional arguments
    https://youtu.be/zF34dRivLOw?t=936

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod print;
+
+// To run the code put the following in the terminal
+// cargo run
+// For a release build do
+// cargo build --release
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    print::run();
 }
---
+++ src/print.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+    // Print to console
+    println!("Hello from print.rs");
+
+    // Basic Formatting
+    println!("{} is {} year old", "Billy", 999);
+
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}", "Billy", "London", "dance");
+}
