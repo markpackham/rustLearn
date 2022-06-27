4e09e6c51fb2e409622fd8fa25b534bc1cc08ca7
mark <markpackham1@gmail.com>
2022-06-27 12:38:35 +0100

    demo memory size of array
    https://youtu.be/zF34dRivLOw?t=3206

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,7 +1,23 @@
-// Arrays - FIXED!!!! list where elements are the same data types
+// Arrays - FIXED size & type!!!! list where elements are the same data types
+
+use std::mem;
 
 pub fn run(){
-    let numbers: [i64; 5] = [1,2,3,4,5];
+    let mut numbers: [i64; 5] = [1,2,3,4,5];
+
+    // Re-assign values - we can't add on to them but we can change them
+    numbers[2] = 300;
 
     println!("{:?}", numbers);
+
+    // Get single value
+    println!("First value {}", numbers[0]);
+
+    // Get length
+    println!("Array length {}", numbers.len());
+
+    // Arrays are stack allocated, we can get their size in bytes
+    // since we use std::mem we can just write "mem::"
+    // println!("Array occupies {} bytes", std::mem::size_of_val(&numbers));
+    println!("Array occupies {} bytes", mem::size_of_val(&numbers));
 }
