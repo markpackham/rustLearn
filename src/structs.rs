c57b7bda76b6f6e5b8292c3a0b5c5bcdc27f2b9a
mark <markpackham1@gmail.com>
2022-06-28 08:16:31 +0100

    demo Traditional Struct
    https://youtu.be/zF34dRivLOw?t=5225

--- src/structs.rs
+++ src/structs.rs
@@ -1,5 +1,22 @@
 // Structs - Used to create custom data types, Struct is the poor man's Class
 
+// Traditional Struct
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
 pub fn run(){
-    
+
+    let mut c = Color {
+        red: 255,
+        green: 0,
+        blue: 0,
+    };
+
+    c.red = 200;
+
+    println!("Color: {} {} {}", c.red, c.green, c.blue);
+
 }
