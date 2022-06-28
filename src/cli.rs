2c16890899bcff7756fdc3b1d0fa3cb70e07fe62
mark <markpackham1@gmail.com>
2022-06-28 10:03:56 +0100

    add command line interface file
    https://youtu.be/zF34dRivLOw?t=6467

---
+++ src/cli.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+pub fn run(){
+    let args: Vec<String> = env::args().collect();
+    let command: String = args[1].clone();
+    let name: &str = "Jim";
+    let status: &str = "100%";
+  
+}
