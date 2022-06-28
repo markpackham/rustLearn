4181d16efd2e7e5dcd1ddd3f1f21d4bbf14353f1
mark <markpackham1@gmail.com>
2022-06-28 11:00:06 +0100

    show input responses in command line

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,22 @@
 use std::env;
 
+// Grabbing terminal input and responding to it
+
 pub fn run(){
     let args: Vec<String> = env::args().collect();
     let command: String = args[1].clone();
     let name: &str = "Jim";
     let status: &str = "100%";
   
+    // to trigger this run
+    // cargo run hello
+    if(command == "hello") {
+        println!("Hi {}, how are you", name);
+    } 
+    // cargo run status
+    else if command == "status" {
+        println!("Status is {}", status);
+    } else{
+        println!("That is not a valid command");
+    }
 }
